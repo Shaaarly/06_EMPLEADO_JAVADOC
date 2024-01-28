6272e69b96fcfa1f874eae3d2a5816a5d80405f7
Carles Tur <carlesturcardona@gmail.com>
2024-01-28 16:24:01 +0100

    Inicial

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"clase.estudiantes","c":"Empleado","l":"Empleado()","u":"%3Cinit%3E()"},{"p":"clase.estudiantes","c":"Empleado","l":"Empleado(String, String, int, double)","u":"%3Cinit%3E(java.lang.String,java.lang.String,int,double)"},{"p":"clase.estudiantes","c":"Empleado","l":"plus(double)"}];updateSearchResults();
